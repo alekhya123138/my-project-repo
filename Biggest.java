976c3f6199aed1edb85084f805e4e41049f4c2c2
alekhya123138 <varanasialekhya234@gmail.com>
2025-08-25 11:27:18 +0530

    Add files via upload

---
+++ Biggest.java
@@ -0,0 +1,39 @@
+class Biggest
+{
+public static void main(String args[])
+{
+int a=10,b=40,c=30;
+if(a>b&&a>c)
+{
+System.out.println("a is big");
+}
+if(b>a&&b>c);
+{
+System.out.println("b is big");
+}
+else
+{
+System.out.println("c is big");
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
